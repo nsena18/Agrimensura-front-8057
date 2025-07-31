4cfe82f1fa4ace4e8019177fbb4d7715615196e2
lmedinx2 <lucianoimm1@gmail.com>
2025-07-31 12:19:10 -0400

    Agregar ABM de tipos de entidades

---
+++ panel/src/views/TiposEntidades/Modal.js
@@ -0,0 +1,201 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import api from "../../api";
+import { formatCurrency } from '../../functions/functions';
+
+import { Row, Col, Label, Input, InputGroup, FormFeedback } from 'reactstrap';
+import moment from 'moment';
+
+import ParadigmaModal from "../../components/ParadigmaModal/ParadigmaModal.js"
+import ParadigmaAsyncSeeker from "../../components/ParadigmaAsyncSeeker/ParadigmaAsyncSeeker.js"
+import ParadigmaLabeledInput from "../../components/ParadigmaLabeledInput/ParadigmaLabeledInput.js"
+import ParadigmaDatePicker from "../../components/ParadigmaDatePicker/ParadigmaDatePicker.js"
+
+class Modal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: null,
+            nombre: '',
+            postVariables: ['nombre'],
+            errors: [],
+        };
+    }
+
+    static propTypes = {
+        onSubmit: PropTypes.func,
+        id: PropTypes.func,
+        action: PropTypes.oneOf(['CREATE', 'EDIT', 'DETAIL', 'DELETE']).isRequired,
+    }
+
+    resetForm() {
+        this.setState({
+            id: null,
+            nombre: '',
+            errors: [],
+        });
+    }
+
+    getData() {
+        const { precioUnitario, postVariables } = this.state;
+        let data = {};
+        postVariables.forEach(x => {
+            data[x] = this.state[x];
+        });
+
+        return data;
+    }
+
+    setData(data) {
+        if (data.success) {
+            this.setState({
+                id: data.id,
+                nombre: data.nombre,
+                debaja: data.debaja || false,
+            });
+        }
+    }
+    setErrors(errors) {
+        this.setState({
+            errors: errors,
+        });
+    }
+
+    onChangeField(field, value) {
+        this.setState(prevState => {
+            let errors = prevState.errors;
+            if (errors) errors[field] = null;
+            prevState.errors = errors;
+            prevState[field] = value;
+            return prevState;
+        });
+    }
+
+    getError(field) {
+        let errors = this.state.errors;
+        if (errors) return errors[field];
+        else return null;
+    }
+
+    modalVars() {
+        const { action } = this.props;
+        if (action === "CREATE") {
+            return {
+                get: false,
+                submitType: "POST",
+                title: "Crear Tipo de Entidad",
+                buttonTitle: "Crear",
+                buttonIcon: "fa fa-plus fa-lg",
+                danger: false,
+
+                successMessage: "Tipo de Entidad creado correctamente.",
+                saveButton: true,
+                saveButtonLabel: "Crear",
+                buttonClass: "",
+                disabled: false,
+            }
+
+        }
+        else if (action === "DETAIL") {
+            return {
+                get: true,
+                submitType: null,
+                title: "Tipo de Entidad",
+                buttonTitle: "Ver",
+                danger: false,
+                buttonIcon: "fa fa-search fa-lg",
+                successMessage: "",
+                cancelButtonLabel: "Cerrar",
+                saveButton: false,
+                buttonClass: "",
+                disabled: true,
+                id: this.props.id,
+            };
+
+        }
+        else if (action === "EDIT") {
+            return {
+                get: true,
+                submitType: "PUT",
+                title: "Editar Tipo de Entidad",
+                buttonTitle: "Editar",
+                danger: false,
+                buttonIcon: "fa fa-pencil fa-lg",
+                successMessage: "El Tipo de Entidad ha sido editado con éxito.",
+                saveButton: true,
+                saveButtonLabel: "Guardar",
+                buttonClass: "",
+                disabled: false,
+                id: this.props.id,
+            };
+
+        }
+        else if (action === "DELETE") {
+            return {
+                get: true,
+                submitType: "DELETE",
+                title: "Eliminar Tipo de Entidad",
+                buttonTitle: "Eliminar",
+                danger: true,
+                buttonIcon: "fa fa-trash fa-lg",
+                successMessage: "El Tipo de Entidad ha sido eliminado con éxito.",
+                saveButton: true,
+                saveButtonLabel: "Eliminar",
+                buttonClass: "",
+                disabled: true,
+                id: this.props.id,
+            };
+
+        }
+    }
+    render() {
+        let vars = this.modalVars();
+        const { action } = this.props;
+        const { nombre } = this.state;
+
+        return (
+            <ParadigmaModal
+                getUrl={(vars.get ? api.comitentes.tiposEntidades : null)}
+                submitUrl={(vars.submitType ? api.comitentes.tiposEntidades : null)}
+                submitType={vars.submitType}
+                id={vars.id}
+
+                onGotData={(data) => this.setData(data)}
+                onGotErrors={(errors) => this.setErrors(errors)}
+                onSubmit={(e) => this.props.onSubmit(e)}
+                onClose={() => this.resetForm()}
+                onPreSubmit={() => this.getData()}
+
+                cancelButtonLabel={vars.cancelButtonLabel}
+
+                title={vars.title}
+
+                danger={vars.danger}
+                buttonClass={vars.buttonClass}
+
+                successMessage={vars.successMessage}
+                missingIdMessage={"Debe seleccionar una fila."}
+                saveButtonLabel={vars.saveButtonLabel}
+                buttonTitle={vars.buttonTitle}
+                buttonIcon={vars.buttonIcon}
+                saveButton={vars.saveButton}
+                closeButton={true}
+
+                escClose={true}
+            >
+
+                <ParadigmaLabeledInput
+                    disabled={vars.disabled}
+                    md={[3, 9]}
+                    label={"Nombre"}
+                    value={nombre}
+                    onChange={(e) => this.onChangeField('nombre', e.target.value)}
+                    error={() => this.getError('nombre')}
+                />
+
+            </ParadigmaModal>
+        );
+    }
+}
+export default Modal;
---
+++ panel/src/views/TiposEntidades/TiposEntidades.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+import api from '../../api/api';
+import { formatCurrency } from '../../functions/functions';
+
+import Modal from "./Modal"
+
+import ParadigmaTable from "../../components/ParadigmaTable/ParadigmaTable";
+import { format } from '../../functions/functions';
+import NumericFilter from '../../components/ParadigmaTable/NumericFilter';
+import DateFilter from "../../components/ParadigmaTable/DateFilter";
+import ParadigmaDatePicker from '../../components/ParadigmaDatePicker/ParadigmaDatePicker';
+import moment from 'moment';
+
+class TiposEntidades extends Component {
+    constructor() {
+        super();
+        this.state = {
+            columns: [
+                {
+                    Header: "id",
+                    id: "id",
+                    accessor: "id",
+                    width: 100,
+                    show: false,
+                    private: true,
+                },
+                {
+                    Header: "Nombre",
+                    id: "nombre",
+                    accessor: "nombre",
+                    show: true,
+                }
+            ]
+        };
+    }
+
+    render() {
+        const { data, pages, loading } = this.state;
+        const outerSort = [
+            {
+                id: 'nombre',
+                desc: false
+            },
+        ];
+        return (
+            <ParadigmaTable
+                buttons={[
+                    {
+                        create: true,
+                        component: (props) => <Modal {...props} action="CREATE" />,
+                        permission: 'tiposentidades_new',
+                    },
+                    {
+                        edit: true,
+                        component: (props) => <Modal {...props} action="EDIT" />,
+                        permission: 'tiposentidades_edit',
+                    },
+                    {
+                        edit: true,
+                        component: (props) => <Modal {...props} action="DETAIL" />,
+                        permission: 'tiposentidades_detail',
+                    },
+                    {
+                        edit: true,
+                        component: (props) => <Modal {...props} action="DELETE" />,
+                        permission: 'tiposentidades_delete',
+                    },
+                ]}
+                apiUrl={api.comitentes.tiposEntidades}
+                columns={this.state.columns}
+                /* exportUrl={api.comitentes.profesiones} */
+                title={"Tipos de Entidades"}
+                outerSort={outerSort}
+            />
+        );
+    }
+}
+
+export default TiposEntidades;
